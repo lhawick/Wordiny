f4d789c29bb5d5603eb99063c5d42e7f8d554406
Sergey Sobol <eskapte@mail.ru>
2025-05-03 23:33:12 +0300

    Setup logging

--- Wordiny.Api/Program.cs
+++ Wordiny.Api/Program.cs
@@ -1,18 +1,21 @@
 using Microsoft.Extensions.Options;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Wordiny.Api.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.Configure<BotConfig>(builder.Configuration.GetSection(nameof(BotConfig)));
-
 // Logging
+
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
+
+// Add services to the container.
+
+builder.Services.Configure<BotConfig>(builder.Configuration.GetSection(nameof(BotConfig)));
 
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>(services =>
@@ -30,7 +33,18 @@ var app = builder.Build();
 
 app.MapPost("/update", (Update update, ILogger<Program> logger, CancellationToken token = default) =>
 {
-    logger.LogInformation(update.Type.ToString());
+
+#if DEBUG
+    var jsonSerializerOptions = new JsonSerializerOptions
+    {
+        WriteIndented = true
+    };
+
+    jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+    var updateAsJson = JsonSerializer.Serialize(update, jsonSerializerOptions);
+    logger.LogDebug($"Received an update event with type {update.Type}\n{updateAsJson}");
+#endif
 
     return Results.Ok();
 });
