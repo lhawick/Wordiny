10f80a475d49d6c4336dda323bfaa27048578322
Sergey Sobol <eskapte@mail.ru>
2025-11-09 14:03:28 +0300

    Обрабатываем ошибки при обработке запросов

---
+++ src/Wordiny.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Data.Common;
+
+namespace Wordiny.Api.Filters;
+
+public class ExceptionFilter : IEndpointFilter
+{
+    private readonly ILogger<ExceptionFilter> _logger;
+
+    public ExceptionFilter(ILogger<ExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        try
+        {
+            return await next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception occured: {errorMessage}", ex.Message);
+
+            return ex is DbException ? Results.InternalServerError() : Results.Ok();
+        }
+    }
+}
