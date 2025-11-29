d17dd21fb62c6398e204f79fdc3418f3e1a19457
lhawick <eskapte@mail.ru>
2025-11-29 23:15:25 +0300

    Setting web hook after app started

--- src/Wordiny.Api/Services/ConfigureWebhookService.cs
+++ src/Wordiny.Api/Services/ConfigureWebhookService.cs
@@ -9,23 +9,38 @@ public class ConfigureWebhookService : IHostedService
 {
     private readonly BotConfig _botConfig;
     private readonly ITelegramBotClient _telegramBotClient;
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly ILogger<ConfigureWebhookService> _logger;
 
-    public ConfigureWebhookService(IOptions<BotConfig> botConfig, ITelegramBotClient telegramBotСlient)
+    public ConfigureWebhookService(
+        IOptions<BotConfig> botConfig,
+        ITelegramBotClient telegramBotСlient,
+        IHostApplicationLifetime hostApplicationLifetime,
+        ILogger<ConfigureWebhookService> logger)
     {
         _botConfig = botConfig.Value;
         _telegramBotClient = telegramBotСlient;
+        _hostApplicationLifetime = hostApplicationLifetime;
+        _logger = logger;
     }
 
     public async Task StartAsync(CancellationToken token = default)
     {
-        await _telegramBotClient.SetWebhook(
-            url: _botConfig.BotWebHookUrl,
-            allowedUpdates: [UpdateType.Message],
-            cancellationToken: token);
+        _hostApplicationLifetime.ApplicationStarted.Register(async () =>
+        {
+            await _telegramBotClient.SetWebhook(
+                url: _botConfig.BotWebHookUrl,
+                allowedUpdates: [UpdateType.Message],
+                cancellationToken: token);
+
+            _logger.LogInformation($"Web hook setted");
+        });
     }
 
     public async Task StopAsync(CancellationToken token = default)
     {
         await _telegramBotClient.DeleteWebhook(cancellationToken: token);
+
+        _logger.LogInformation($"Web hook deleted");
     }
 }
